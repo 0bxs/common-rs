4e87bc9b7f430b84ff69d92b9727fa70ef589869
xc <1430158983@qq.com>
2026-02-14 17:09:13 +0800

    redis version update

--- src/redis/mod.rs
+++ src/redis/mod.rs
@@ -1,8 +1,5 @@
-use redis::ConnectionAddr::Tcp;
 use redis::aio::MultiplexedConnection;
-use redis::{
-    AsyncTypedCommands, Client, ConnectionInfo, ProtocolVersion, RedisConnectionInfo, RedisResult,
-};
+use redis::{AsyncTypedCommands, Client, RedisResult};
 use std::error::Error;
 use std::sync::OnceLock;
 use tracing::info;
@@ -10,15 +7,10 @@ use tracing::info;
 static CLIENT: OnceLock<Client> = OnceLock::new();
 
 pub async fn init(conf: Redis) -> Result<(), Box<dyn Error>> {
-    let c = Client::open(ConnectionInfo {
-        addr: Tcp(conf.addr.clone(), conf.port),
-        redis: RedisConnectionInfo {
-            db: conf.db,
-            username: conf.username.clone(),
-            password: conf.password.clone(),
-            protocol: ProtocolVersion::RESP3,
-        },
-    })?;
+    let c = Client::open(format!(
+        "redis://{}:{}@{}:{}/{} ",
+        conf.username, conf.password, conf.addr, conf.port, conf.db
+    ))?;
     CLIENT.set(c).unwrap();
     info!("Connected to Redis：{}", redis().await?.ping().await?);
     Ok(())
@@ -29,13 +21,13 @@ fn client() -> &'static Client {
 }
 
 pub async fn redis() -> RedisResult<MultiplexedConnection> {
-    client().get_multiplexed_tokio_connection().await
+    client().get_multiplexed_async_connection().await
 }
 
 pub struct Redis {
     pub addr: String,
-    pub username: Option<String>,
-    pub password: Option<String>,
+    pub username: String,
+    pub password: String,
     pub port: u16,
     pub db: i64,
 }
