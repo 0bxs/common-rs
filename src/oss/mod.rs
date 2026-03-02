6082e00e7a4efab7af65cc4272bd2026aeebee54
xc <1430158983@qq.com>
2026-03-02 16:43:10 +0800

    oss build

--- src/oss/mod.rs
+++ src/oss/mod.rs
@@ -14,30 +14,43 @@ pub struct Oss {
 
 static BUCKET: OnceLock<OSS> = OnceLock::new();
 
-static BUILDER: OnceLock<RequestBuilder> = OnceLock::new();
+static PUT_BUILDER: OnceLock<RequestBuilder> = OnceLock::new();
+
+static GET_BUILDER: OnceLock<RequestBuilder> = OnceLock::new();
 
 pub fn init(conf: Oss) {
     let endpoint = conf.endpoint;
     BUCKET.set(OSS::new(conf.access_key, conf.secret_key, endpoint.clone(), conf.bucket)).unwrap();
+    let mut builder = RequestBuilder::new().with_expire(60).
+        with_content_type("text/plain");
+    if conf.cdn {
+        builder.cdn = Some(endpoint.clone());
+    }
+    PUT_BUILDER.set(builder).unwrap();
+
     let mut builder = RequestBuilder::new().with_expire(60);
     if conf.cdn {
         builder.cdn = Some(endpoint);
     }
-    BUILDER.set(builder).unwrap();
+    GET_BUILDER.set(builder).unwrap();
 }
 
 fn bucket() -> &'static OSS {
     BUCKET.get().unwrap()
 }
 
-fn builder() -> &'static RequestBuilder {
-    BUILDER.get().unwrap()
+fn put_builder() -> &'static RequestBuilder {
+    PUT_BUILDER.get().unwrap()
+}
+
+fn get_builder() -> &'static RequestBuilder {
+    GET_BUILDER.get().unwrap()
 }
 
 pub fn put_auth_url(file_path: &str) -> String {
-    bucket().sign_download_url(file_path, builder())
+    bucket().sign_upload_url(file_path, put_builder())
 }
 
 pub fn get_auth_url(file_path: &str) -> String {
-    bucket().sign_upload_url(file_path, builder())
+    bucket().sign_download_url(file_path, get_builder())
 }
