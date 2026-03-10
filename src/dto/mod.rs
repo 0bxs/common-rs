e7343936b086ad03b71035b6f5244608cf07729b
xc <1430158983@qq.com>
2026-03-10 14:23:33 +0800

    id page

--- src/dto/mod.rs
+++ src/dto/mod.rs
@@ -41,3 +41,24 @@ pub enum OrderType {
     ASC,
     DESC,
 }
+
+#[macro_export]
+macro_rules! id_page_dto {
+    ($name:ident { $($field:tt)* }) => {
+        #[derive(Deserialize, Validate)]
+        pub struct $name {
+            #[serde(flatten)]
+            #[validate(nested)]
+            pub page: IdPageDto,
+            $($field)*
+        }
+    };
+}
+
+#[derive(Deserialize, Validate)]
+pub struct IdPageDto {
+    #[validate(range(min = 0))]
+    pub last_id: i64,
+    #[validate(range(min = 1))]
+    pub page_size: u64,
+}
